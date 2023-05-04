87a0b6c5b2090c46ade62ef5591544906e7d3832
omaks45 <danielgozie5@gmail.com>
2023-05-04 00:02:57 +0000

    2-measure_runtime.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,6 +13,6 @@ from typing import Generator
 
 async def async_generator() -> Generator[float, None, None]:
     """Loop 10 times, wait 1 sec each time"""
-    for i in range(10):
+    for idx in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
