2185e8fb4b119bc11311e6f24e45adda3805b8ff
omaks45 <danielgozie5@gmail.com>
2023-05-17 02:56:49 +0000

    2-floor.py

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type annotation of float function
+"""
+
+
+def floor(n: float) -> int:
+    """
+    returns the returns the floor of the float
+    """
+    return int(n)
