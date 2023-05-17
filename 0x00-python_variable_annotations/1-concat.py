c1e225680be4326b8393d1e9324914f25614e826
omaks45 <danielgozie5@gmail.com>
2023-05-17 02:19:36 +0000

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type annotation on concatenation
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenate two strings
+    """
+    return ("{}{}".format(str1, str2))
