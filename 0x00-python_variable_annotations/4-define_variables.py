351dce29c67ca32ada322fb9d7fcacb16bc1963d
omaks45 <danielgozie5@gmail.com>
2023-05-17 04:15:44 +0000

    4-define_variables.py

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -4,7 +4,7 @@ variable annotation
 """
 
 
-a = 1
-pi = 3.14
-i_understand_annotations = True
-school = "Holberton"
+a: str = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
