869fbcbfcf34a5e98b6ca61d918453fdc99904c1
omaks45 <danielgozie5@gmail.com>
2023-05-17 01:56:24 +0000

    0-add.py

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type annotation function
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    addition of two numbers that returns float
+    """
+    return a + b
