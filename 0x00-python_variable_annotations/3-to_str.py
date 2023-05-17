df6fa9d7efa3080f8383d37c107bedb117ee1e8c
omaks45 <danielgozie5@gmail.com>
2023-05-17 03:08:31 +0000

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+type annotation function that turns a float to string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    returns string rep of  the float
+    """
+    return str(n)
