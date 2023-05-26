9987689be971caf0cdf70a81edf9f5381a65929d
omaks45 <danielgozie5@gmail.com>
2023-05-26 00:03:59 +0000

    test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -31,6 +31,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand(
         [
             ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a"), {"b": 2}),
             ({"a": {"b": 2}}, ("a", "b"), 2)
         ]
     )
